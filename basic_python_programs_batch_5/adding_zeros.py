56dde5b28a55d9495958e768986b8afc7fb06042
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-20 22:26:52 +0800

    Added f-string and modify it make a 6-digit number

--- basic_python_programs_batch_5/adding_zeros.py
+++ basic_python_programs_batch_5/adding_zeros.py
@@ -1,4 +1,5 @@
 #Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 #Ask for user input
 inputed_number = int(input("Enter a number from 0-1000: "))
-
+#Fill the number with zero
+print(f"{inputed_number:6}")
