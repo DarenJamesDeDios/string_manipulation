9f71f63f3caba7e4a9dd2ec48c25db14f5fa29d0
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-23 11:56:09 +0800

    Added len and split so that it could only count the words

--- basic_python_programs_batch_5/check_number_of_words.py
+++ basic_python_programs_batch_5/check_number_of_words.py
@@ -1,3 +1,5 @@
 #Create a program that ask the user to input a complete statement. Print the number of words in the input.
 #Ask for user input
 inputed_statement = input("Enter a statement: ")
+
+print(len(inputed_statement.split()))
