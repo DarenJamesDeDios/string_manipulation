4ec19cb55cd895989e402114182df84791a75775
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-23 12:01:55 +0800

    Added title to print the desired output

--- basic_python_programs_batch_5/proper_casing_letters.py
+++ basic_python_programs_batch_5/proper_casing_letters.py
@@ -1,3 +1,5 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 #Ask for user input
 inputed_fullname = input("Enter your fullname: ")
+
+print(inputed_fullname.title())
