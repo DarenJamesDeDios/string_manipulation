fc2f5aca2688e8320e1e8fa185ca0a0c4dfdf0ac
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-23 12:03:32 +0800

    Added swapcase to make any lowercase letters into uppercase and vice versa

--- basic_python_programs_batch_5/reverse_casing.py
+++ basic_python_programs_batch_5/reverse_casing.py
@@ -1,3 +1,5 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 #Ask for user input
 inputed_fullname = input("Enter your fullname: ")
+
+print(inputed_fullname.swapcase())
