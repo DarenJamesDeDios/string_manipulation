68b07dcaa8887d23a8d73a9d33dc52797d77094f
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-23 12:09:15 +0800

    Added lstrip function to remove excess spaces in the left side

--- basic_python_programs_batch_5/space_name_corrector_string.py
+++ basic_python_programs_batch_5/space_name_corrector_string.py
@@ -1,3 +1,5 @@
 #Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning
 #Ask for user Input
 inputed_fullname = input("Enter your fullname (include several spaces): ")
+
+print(inputed_fullname.lstrip())
