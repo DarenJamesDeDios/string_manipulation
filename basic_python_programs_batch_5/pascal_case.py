3d8e997a00e5b336b6177d76bdddde2c3d7d13b6
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-23 12:00:49 +0800

    Added title function with replace space in order to make a pascal case

--- basic_python_programs_batch_5/pascal_case.py
+++ basic_python_programs_batch_5/pascal_case.py
@@ -1,3 +1,5 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 #Ask for user input
 inputed_fullname = input("Enter your fullname: ")
+
+print(inputed_fullname.title().replace(" ",""))
