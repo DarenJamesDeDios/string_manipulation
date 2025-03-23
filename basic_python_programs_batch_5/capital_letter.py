b2cf60377f64701c81c39b394d1eea44a4d8050c
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-23 11:46:53 +0800

    Changed my f-string to z.fill but results to an error

--- basic_python_programs_batch_5/capital_letter.py
+++ basic_python_programs_batch_5/capital_letter.py
@@ -1,3 +1,4 @@
 #Create a program that ask the user to input their fullname. Print the input in all capital letter.
 #Ask for user input
 inputed_fullname = input("Enter your fullname: ")
+print(inputed_fullname.capitalize())
