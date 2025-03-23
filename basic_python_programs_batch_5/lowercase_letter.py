a3aa4ef9ce572992d01211ea7bb26ea32c39a937
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-23 11:58:47 +0800

    Added lower function to lowercase all letters

--- basic_python_programs_batch_5/lowercase_letter.py
+++ basic_python_programs_batch_5/lowercase_letter.py
@@ -1,3 +1,5 @@
 #Create a program that ask the user to input their fullname. Print the input in all lower case.
 #Ask for user input
 inputed_fullname = input("Enter your fullname: ")
+
+print(inputed_fullname.lower())
