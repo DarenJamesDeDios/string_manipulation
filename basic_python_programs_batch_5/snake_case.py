605e49ac9eb48eaa80e6c57bfbfb81f6ec4a72da
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-23 12:06:23 +0800

    Added lower function and replace function to print a snake case

--- basic_python_programs_batch_5/snake_case.py
+++ basic_python_programs_batch_5/snake_case.py
@@ -1,3 +1,5 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 #Ask for user input
 inputed_fullname = input("Enter your fullname(make it in incorrect casing): ")
+
+print(inputed_fullname.lower().replace(" ","_"))
