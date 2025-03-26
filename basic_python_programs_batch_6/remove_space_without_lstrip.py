f33fa7580079c11d7156b25e2a2943e11409e4d2
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-26 22:45:58 +0800

    Trying add it again

---
+++ basic_python_programs_batch_6/remove_space_without_lstrip.py
@@ -0,0 +1,4 @@
+#lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+inputed_statement = input("Enter words with spaces in the beginning: ")
+
+print(inputed_statement.replace(" ",""))
