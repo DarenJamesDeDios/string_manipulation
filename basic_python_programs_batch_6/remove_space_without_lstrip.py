d1b5ca54bd83f37273503be30164ede187f91d7c
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-27 15:59:09 +0800

    replaced the user_input and replace function to make things easier to understand

--- basic_python_programs_batch_6/remove_space_without_lstrip.py
+++ basic_python_programs_batch_6/remove_space_without_lstrip.py
@@ -1,4 +1,4 @@
 #lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 inputed_statement = input("Enter words with spaces in the beginning: ")
 
-print(inputed_statement.replace(" ",""))
+print(inputed_statement.replace(" ","").replace(""," "))
