75d81fc316d41ebf3cde3d9a825f1b8e8143c08d
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-27 20:33:18 +0800

    Added len function and index to stop the while loop when it encounter non space string for the first time

--- basic_python_programs_batch_6/remove_space_without_lstrip.py
+++ basic_python_programs_batch_6/remove_space_without_lstrip.py
@@ -1,11 +1,10 @@
 #lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 #initialization
-text_string = "        I am a programmer"
+text_string = "    I am a programmer"
 index_string = 0
 #need to make a block of code that remove spaces but stop when the it is a letter
-while text_string ==' ':
+while index_string < len(text_string) and text_string[index_string] == ' ':
     index_string += 1
-    if text_string !=' ':
-        break
+    
 mylstripv = text_string[index_string:]
 print(mylstripv)
