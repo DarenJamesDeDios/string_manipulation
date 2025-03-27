5c052ec415524a045f5c68ca277213717b367d4e
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-27 16:06:02 +0800

    Tried using while loop

--- basic_python_programs_batch_6/remove_space_without_lstrip.py
+++ basic_python_programs_batch_6/remove_space_without_lstrip.py
@@ -1,4 +1,11 @@
 #lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
-inputed_statement = input("Enter words with spaces in the beginning: ")
-
-print(inputed_statement.replace(" ","").replace(""," "))
+#initialization
+text_string = "        I am a programmer"
+index_string = 0
+#need to make a block of code that remove spaces but stop when the it is a letter
+while text_string ==' ':
+    index_string += 1
+    if text_string !=' ':
+        break
+mylstripv = text_string[index_string:]
+print(mylstripv)
