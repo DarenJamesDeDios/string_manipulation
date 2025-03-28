d271bf1a16ff09f08050ed1b7840b4340f867e51
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-28 15:47:02 +0800

    Added strings and prefix to remove for manually removing prefix

---
+++ basic_python_programs_batch_6/without_lower_function.py
@@ -0,0 +1,5 @@
+#lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+#initialization
+string = "Daren James De Dios"
+prefixremove = "Dar"
+
