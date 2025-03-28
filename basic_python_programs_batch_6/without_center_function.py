2f5315f0aef0068e4e1a3b776e066331c4a140c9
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-28 16:24:27 +0800

    Calculate the total padding required

--- basic_python_programs_batch_6/without_center_function.py
+++ basic_python_programs_batch_6/without_center_function.py
@@ -1,16 +1,8 @@
 #center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function
-text = "Hello"
+text = "Greetings"
 width = 20
 fillchar = " "
-# Calculate the total padding required
+
 total_padding = width - len(text)
 
-# If the width is smaller than the text, just use the text as it is
-if total_padding > 0:
-    left_padding = total_padding // 2
-    right_padding = total_padding - left_padding
-    centered_text = fillchar * left_padding + text + fillchar * right_padding
-else:
-    centered_text = text
 
-print(centered_text)
