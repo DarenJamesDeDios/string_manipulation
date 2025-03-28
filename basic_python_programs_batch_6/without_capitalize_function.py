d3c3ee94d80422540442e8a48a38fa545ceb07a0
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-28 08:14:39 +0800

    Added upper function to capitalize only the first word lower the rest with the lower function

--- basic_python_programs_batch_6/without_capitalize_function.py
+++ basic_python_programs_batch_6/without_capitalize_function.py
@@ -1,2 +1,6 @@
 #capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 string = "Hello, EvEryONe"
+
+capitalized_string = string[0].upper() + string[1:].lower()
+ 
+print(capitalized_string)
