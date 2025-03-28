89f8578c1ecaac7a57528585fe0c43dab7642d7e
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-28 10:15:39 +0800

    Added capitalize function so that each word in words are capitalize in the first letter

--- basic_python_programs_batch_6/without_using_title_function.py
+++ basic_python_programs_batch_6/without_using_title_function.py
@@ -1,4 +1,8 @@
 #title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 string = "How are you?"
-title_string = string.split()
+words = string.split()  # Split the string into words
+title_string = ""
+for word in words:
+    title_string += word.capitalize()  # Capitalize each word and add a space after it
+
 print(title_string)
