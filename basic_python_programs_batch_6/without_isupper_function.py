e2e04de173adc8fb2df44ecb5fd4aa5f115560d5
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-28 15:19:02 +0800

    Added a user_input so that it would depend on the user to check if there word/statemnt is in all uppercase

---
+++ basic_python_programs_batch_6/without_isupper_function.py
@@ -0,0 +1,10 @@
+#isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+string = input("Enter a statement/word: ")
+uppercase_letters = 0
+for letter in string:
+    if letter.islower():
+        uppercase_letters += 1
+if len(string) == uppercase_letters:
+    print("All letters are uppercase")
+else:
+    print("Not all letters are uppercase")
