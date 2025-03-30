587665a0dfc5f4d6de23aa75d5f9d3e8dedd1f05
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-30 20:00:34 +0800

    Added user input for more interaction

--- basic_python_programs_batch_7/prog07.py
+++
@@ -1,2 +0,0 @@
-#zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
-#planning to use f-strings instead
