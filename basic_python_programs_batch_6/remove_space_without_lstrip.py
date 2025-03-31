0cc1f316b4a2f94598bdf39fc6d0fc8236513267
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 10:14:50 +0800

    Fix some files that are not running

--- basic_python_programs_batch_6/remove_space_without_lstrip.py
+++ basic_python_programs_batch_6/remove_space_without_lstrip.py
@@ -1,10 +1,7 @@
 #lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 #initialization
 text_string = "    I am a programmer"
-index_string = 0
+index_string = len(text_string) - 1 #made this so it could start at the end of the string
 #need to make a block of code that remove spaces but stop when the it is a letter
-while index_string < len(text_string) and text_string[index_string] == ' ':
-    index_string += 1
-    
-mylstripv = text_string[index_string:]
-print(mylstripv)
+while index_string >= 0 and text_string[index_string] == ' ':
+    index_string -= 1
--- basic_python_programs_batch_7/remove_space_without_rstrip.py
+++ basic_python_programs_batch_7/remove_space_without_rstrip.py
@@ -1,6 +1,7 @@
 #rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 #initialized the strings and values
-
+text_string = "Using python language      "
+index_string = 0
 #a block of code that checks the trailing spaces of the string
 
 #printting the desired output
