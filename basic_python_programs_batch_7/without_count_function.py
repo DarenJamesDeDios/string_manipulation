c41aa83b717331204c6dbe5d7147503044620b52
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 14:03:35 +0800

    used for loop to split the strings into words and put them into an index

--- basic_python_programs_batch_7/without_count_function.py
+++ basic_python_programs_batch_7/without_count_function.py
@@ -1,4 +1,9 @@
 #count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 #initialized the strings and values
-statement = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
-string = "woodchuck"
+letter = 'woodchuck'
+String = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
+List = []
+#used for loop to split the strings into words and put them into an index
+for i in String.split():
+    List.append(i)
+
