e3facc01c69e2341aa6282f3cc66d970b41b3988
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 16:23:03 +0800

    Renamed a file and initialized strings, added the for loop in order to check starting from the end

--- basic_python_programs_batch_7/without_rindex_function.py
+++ basic_python_programs_batch_7/without_rindex_function.py
@@ -1,6 +1,8 @@
 #rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 #initialized the strings and values
+string = "hello world, hello universe"
+substring = "hello"
 
-#a block of code that checks the location starting from the last character
+#loop through the string starting from the end
+for i in range(len(string) - len(substring), -1, -1):
 
-#print the firt location
