f1e1f5e1378ea27e4ba854776612f9ac59061351
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 13:38:24 +0800

    Renamed the file and initialized values and strings

--- basic_python_programs_batch_7/without_count_function.py
+++ basic_python_programs_batch_7/without_count_function.py
@@ -1,6 +1,4 @@
 #count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 #initialized the strings and values
-
-#a block of code that checks how many times the word has appeared
-
-#print the desired output
+statement = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
+string = "woodchuck"
