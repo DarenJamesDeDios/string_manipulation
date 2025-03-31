f04b738e14d144bb28f3958c8d7a468af033cee9
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 10:28:14 +0800

    Changed it back to its original state

--- basic_python_programs_batch_7/remove_space_without_rstrip.py
+++ basic_python_programs_batch_7/remove_space_without_rstrip.py
@@ -1,9 +1,9 @@
 #rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 #initialized the strings and values
-text_string = "Using python language******"
+text_string = "Using python language     "
 index_string = len(text_string) - 1
 #a block of code that checks the trailing spaces of the string
-while index_string >= 0 and text_string[index_string] == '*':
+while index_string >= 0 and text_string[index_string] == ' ':
     index_string -= 1
 #printting the desired output
 myrstripv = text_string[:index_string + 1] # Slice the string up to the last non-space character
