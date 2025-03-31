b34356d8dfd6c0d31999feebe8ddaccb0213a9c8
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 13:19:22 +0800

    Initialized strings and values

--- basic_python_programs_batch_7/prog06.py
+++ basic_python_programs_batch_7/prog06.py
@@ -1,6 +1,6 @@
 #rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 #initialized the strings and values
 
-#a block of code that adds space at the beginning of the string
-
-#printting the desired output
+string = "Lustre"
+width = 10
+fill_char = " "
