40031ffe926784e4f46b3a1b8024a8cfa4fb13cd
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 10:15:52 +0800

    fix the mix-up of the files

--- basic_python_programs_batch_6/remove_space_without_lstrip.py
+++ basic_python_programs_batch_6/remove_space_without_lstrip.py
@@ -1,7 +1,11 @@
 #lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 #initialization
-text_string = "    I am a programmer"
+text_string = "I am a programmer      "
 index_string = len(text_string) - 1 #made this so it could start at the end of the string
 #need to make a block of code that remove spaces but stop when the it is a letter
 while index_string >= 0 and text_string[index_string] == ' ':
     index_string -= 1
+
+#slice the string up to the last non-space character
+rstrip = text_string[:index_string + 1]
+print(rstrip)
