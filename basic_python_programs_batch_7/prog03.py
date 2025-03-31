cc7b9a46219ccece64c9149a73e5163d99ed4b52
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 10:52:39 +0800

    Initialized the values and strings for demonstration

--- basic_python_programs_batch_7/prog03.py
+++ basic_python_programs_batch_7/prog03.py
@@ -1,6 +1,5 @@
 #upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 #initialized the strings and values
+string = "Daren James De Dios"
+result = ""
 
-#a block of code that checks for lowercase letters and converts them to uppercase letters
-
-#print the uppercased strings
