9c68fe43da0c88f418696f78ae60f7407ca09dc2
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 10:53:39 +0800

    used chr and ord to convert them into uppercase letters and manage to get the output

--- basic_python_programs_batch_7/prog03.py
+++ basic_python_programs_batch_7/prog03.py
@@ -2,4 +2,11 @@
 #initialized the strings and values
 string = "Daren James De Dios"
 result = ""
-
+#a block of code that checks lowercase letters and converts them to uppercase letters
+for letter in string:
+    if 'a'<= letter <= 'z' :
+        result += chr(ord(letter) -32) 
+    else:
+        result += letter
+#print the result
+print(result)
