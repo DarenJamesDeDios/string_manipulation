150b2098594022ab1379072f57a142257fa3a2ee
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 10:42:35 +0800

    Fix naming of variables and added if-else function to check suffix

--- basic_python_programs_batch_7/without_removesuffix_.py
+++ basic_python_programs_batch_7/without_removesuffix_.py
@@ -1,10 +1,10 @@
 # removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function
+#initialized the strings and values
 string =  "Daren James De Dios"
 suffix_remove = "ios"
-
+#a block of code that checks if the string has the suffix in the end of the string
 if string.endswith(suffix_remove):
-    result = string[:-len(suffix_remove)]
+    result = string[-len(suffix_remove)]  #checks if the string ends with the specified suffix
 else:
     result = string
-
-print(result)
+#printting the result
