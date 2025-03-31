8b770d1c711ab4d1ff758b7697521085f2cc5838
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-31 16:19:18 +0800

    Added another to for comparing the strings to the specified word

--- basic_python_programs_batch_7/without_index_function.py
+++ basic_python_programs_batch_7/without_index_function.py
@@ -6,4 +6,12 @@ substring = "seashells"
 for i in range(len(string) - len(substring) + 1):
     # Check each character by comparing one by one
     match_found = True
-   
+    for j in range(len(substring)):
+        if string[i + j] != substring[j]:
+            match_found = False
+            break
+    if match_found:
+        print(f"The substring '{substring}' is found at index {i}.")
+        break
+else:
+    print(f"The substring '{substring}' is not found.")
